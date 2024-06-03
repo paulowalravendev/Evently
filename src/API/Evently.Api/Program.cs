fd41746af84acc3f282a7ec8fae7fca1f66fc7c5
Paulo Walraven <pmwc04@gmail.com>
2024-06-03 01:22:19 -0300

    resolve CartService in Ticketing.Infrastructure and change AddTicketingModule signature

--- src/API/Evently.Api/Program.cs
+++ src/API/Evently.Api/Program.cs
@@ -47,7 +47,7 @@ builder.Services.AddHealthChecks()
 
 builder.Services.AddEventsModule(builder.Configuration);
 builder.Services.AddUsersModule(builder.Configuration);
-builder.Services.AddTicketingModule();
+builder.Services.AddTicketingModule(builder.Configuration);
 
 WebApplication app = builder.Build();
 
--- src/Modules/Ticketing/Evently.Modules.Ticketing.Infrastructure/TicketingModule.cs
+++ src/Modules/Ticketing/Evently.Modules.Ticketing.Infrastructure/TicketingModule.cs
@@ -1,4 +1,6 @@
 ﻿using Evently.Common.Presentation.Endpoints;
+using Evently.Modules.Ticketing.Application.Carts;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Evently.Modules.Ticketing.Infrastructure;
@@ -6,10 +8,24 @@ namespace Evently.Modules.Ticketing.Infrastructure;
 public static class TicketingModule
 {
     public static IServiceCollection AddTicketingModule(
-        this IServiceCollection services)
+        this IServiceCollection services,
+        IConfiguration configuration)
     {
+        services.AddInfrastructure(configuration);
+
         services.AddEndpoints(Presentation.AssemblyReference.Assembly);
 
         return services;
+    }
+
+#pragma warning disable S1172 // Unused method parameters should be removed
+#pragma warning disable IDE0060 // Remove unused parameter
+    private static void AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+#pragma warning restore IDE0060 // Remove unused parameter
+#pragma warning restore S1172 // Unused method parameters should be removed
+    {
+        services.AddSingleton<CartService>();
     }
 }
