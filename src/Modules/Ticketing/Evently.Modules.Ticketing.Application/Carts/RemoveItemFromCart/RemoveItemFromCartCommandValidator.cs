c4174f867031f495d8beb9fc3ae6e1e1fc7f95b2
Paulo Walraven <pmwc04@gmail.com>
2024-06-03 18:14:36 -0300

    add use case RemoveItemFromCart in Ticketing.Application

---
+++ src/Modules/Ticketing/Evently.Modules.Ticketing.Application/Carts/RemoveItemFromCart/RemoveItemFromCartCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Evently.Modules.Ticketing.Application.Carts.RemoveItemFromCart;
+
+internal sealed class RemoveItemFromCartCommandValidator 
+    : AbstractValidator<RemoveItemFromCartCommand>
+{
+    public RemoveItemFromCartCommandValidator()
+    {
+        RuleFor(c => c.CustomerId).NotEmpty();
+        RuleFor(c => c.TicketTypeId).NotEmpty();
+    }
+}
