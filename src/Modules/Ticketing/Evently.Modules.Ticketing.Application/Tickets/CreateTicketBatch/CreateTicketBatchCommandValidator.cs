ff099fbba15235be85d840bbca27d0c3ac29e7ec
Paulo Walraven <pmwc04@gmail.com>
2024-06-03 18:31:06 -0300

    add use case CreateTicektBatch in Ticketing.Application

---
+++ src/Modules/Ticketing/Evently.Modules.Ticketing.Application/Tickets/CreateTicketBatch/CreateTicketBatchCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Evently.Modules.Ticketing.Application.Tickets.CreateTicketBatch;
+
+internal sealed class CreateTicketBatchCommandValidator : 
+    AbstractValidator<CreateTicketBatchCommand>
+{
+    public CreateTicketBatchCommandValidator()
+    {
+        RuleFor(c => c.OrderId).NotEmpty();
+    }
+}
