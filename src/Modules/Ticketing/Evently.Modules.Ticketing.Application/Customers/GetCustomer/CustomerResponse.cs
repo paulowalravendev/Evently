1070fba3233d3adfccd79ac25f3cf27144920cf8
Paulo Walraven <pmwc04@gmail.com>
2024-06-03 16:30:05 -0300

    create use cases in Ticketing.Application: CreateCustomer, GetCustomer, UpdateCustomer

---
+++ src/Modules/Ticketing/Evently.Modules.Ticketing.Application/Customers/GetCustomer/CustomerResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Evently.Modules.Ticketing.Application.Customers.GetCustomer;
+
+public sealed record CustomerResponse(
+    Guid Id,
+    string Email,
+    string FirstName,
+    string LastName);
---
+++ src/Modules/Ticketing/Evently.Modules.Ticketing.Application/Customers/UpdateCustomer/UpdateCustomerCommand.cs
@@ -0,0 +1,8 @@
+﻿using Evently.Common.Application.Messaging;
+
+namespace Evently.Modules.Ticketing.Application.Customers.UpdateCustomer;
+
+public sealed record UpdateCustomerCommand(
+    Guid CustomerId,
+    string FirstName,
+    string LastName) : ICommand;
