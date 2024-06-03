16cce49bad1a3bc141ca947ddbb220fd3f146e7e
Paulo Walraven <pmwc04@gmail.com>
2024-06-03 01:14:33 -0300

    create Ticketing.Application with use case AddItemToCart using user and events public api

---
+++ src/Modules/Ticketing/Evently.Modules.Ticketing.Application/Carts/AddItemToCart/AddItemToCartCommand.cs
@@ -0,0 +1,8 @@
+﻿using Evently.Common.Application.Messaging;
+
+namespace Evently.Modules.Ticketing.Application.Carts.AddItemToCart;
+
+public sealed record AddItemToCartCommand(
+    Guid CustomerId,
+    Guid TicketTypeId,
+    decimal Quantity) : ICommand;
