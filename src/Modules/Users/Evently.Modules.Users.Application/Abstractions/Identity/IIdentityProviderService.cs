4b06920ec6f8ab09896c11d826293669ceea4de4
Paulo Walraven <pmwc04@gmail.com>
2024-06-03 22:46:29 -0300

    add identityId for User entity in Users.Domain, use abstraction IdentityProvider to create a identity provider id

---
+++ src/Modules/Users/Evently.Modules.Users.Application/Abstractions/Identity/IIdentityProviderService.cs
@@ -0,0 +1,10 @@
+﻿using Evently.Common.Domain;
+
+namespace Evently.Modules.Users.Application.Abstractions.Identity;
+
+public interface IIdentityProviderService
+{
+    Task<Result<string>> RegisterUserAsync(
+        UserModel user, 
+        CancellationToken cancellationToken = default);
+}
