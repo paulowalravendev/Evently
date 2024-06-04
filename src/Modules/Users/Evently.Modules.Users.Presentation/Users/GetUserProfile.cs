f4f4e56c700c846d5b6a85dd619ea5d0c529bd69
Paulo Walraven <pmwc04@gmail.com>
2024-06-04 01:17:00 -0300

    create ClaimsPrincipalExtensions and apply in presentation for GetUserProfile in Users module

--- src/Modules/Users/Evently.Modules.Users.Presentation/Users/GetUserProfile.cs
+++ src/Modules/Users/Evently.Modules.Users.Presentation/Users/GetUserProfile.cs
@@ -1,4 +1,6 @@
-﻿using Evently.Common.Domain;
+﻿using System.Security.Claims;
+using Evently.Common.Domain;
+using Evently.Common.Infrastructure.Authentication;
 using Evently.Common.Presentation.ApiResults;
 using Evently.Common.Presentation.Endpoints;
 using Evently.Modules.Users.Application.Users.GetUser;
@@ -9,14 +11,14 @@ using Microsoft.AspNetCore.Routing;
 
 namespace Evently.Modules.Users.Presentation.Users;
 
-
 internal sealed class GetUserProfile : IEndpoint
 {
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
-        app.MapGet("users/{id}/profile", async (Guid id, ISender sender) =>
+        app.MapGet("users/profile", async (ClaimsPrincipal claims, ISender sender) =>
         {
-            Result<UserResponse> result = await sender.Send(new GetUserQuery(id));
+            Result<UserResponse> result = await sender
+                .Send(new GetUserQuery(claims.GetUserId()));
 
             return result.Match(Results.Ok, ApiResults.Problem);
         })
